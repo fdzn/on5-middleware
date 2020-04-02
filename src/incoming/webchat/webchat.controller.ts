362507a43b1ee9496d94e4a7d48d3dd1b2f895bb
Akhmad Faudzan Bakri <faudzanbakri@gmail.com>
2020-04-02 23:19:45 +0700

    initial livechat dimas

--- src/incoming/webchat/webchat.controller.ts
+++ src/incoming/webchat/webchat.controller.ts
@@ -5,6 +5,14 @@ import { WebchatService } from './webchat.service';
 export class WebchatController {
   constructor(private readonly webchatService: WebchatService) {}
 
+  @Post()
+  async general(@Body() dataPost: any, @Res() res: Response) {
+    console.log('incoming webchat dimas', dataPost);
+    // const result = await this.webchatService.bot(dataPost);
+    // res.status(result.statusCode).send(result);
+    res.status(200).send(dataPost);
+  }
+
   @Post('bot')
   async bot(@Body() dataPost: any, @Res() res: Response) {
     console.log('incoming webchat BOT', dataPost);
