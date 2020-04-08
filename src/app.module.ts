b6b3b0b63e4202523ffe10ac9ec0f2a7b63f13d8
Akhmad Faudzan Bakri <faudzanbakri@gmail.com>
2020-04-08 21:36:28 +0700

    minio v1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
 import { AppController } from './app.controller';
 import { IncomingModule } from './incoming/incoming.module';
-import { MinioModule } from './minio/minio.module';
+import { MinioNestModule } from './minio/minio.module';
 @Module({
-  imports: [ConfigModule.forRoot(), IncomingModule, MinioModule],
+  imports: [ConfigModule.forRoot(), IncomingModule, MinioNestModule],
   controllers: [AppController],
 })
 export class AppModule {}
