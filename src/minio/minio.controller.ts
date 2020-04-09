79907b86f2695a3310f7f048b2754fbda9bae533
Akhmad Faudzan Bakri <faudzanbakri@gmail.com>
2020-04-09 10:25:48 +0700

    minio multi upload & encrypt fix

--- src/minio/minio.controller.ts
+++ src/minio/minio.controller.ts
@@ -5,23 +5,24 @@ import {
   Res,
   UseInterceptors,
   UploadedFile,
+  UploadedFiles,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { MinioNestService } from './minio.service';
 import { BufferedFile } from './dto/file.model';
 import { UploadPost, DownloadPost } from './dto/minio.dto';
 @Controller('minio')
 export class MinioController {
   constructor(private minioNestService: MinioNestService) {}
-  @Post('upload/single')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadSingle(
-    @UploadedFile() file: BufferedFile,
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('files'))
+  async upload(
+    @UploadedFiles() files: BufferedFile,
     @Body() dataPost: UploadPost,
     @Res() res: Response,
   ) {
-    let result = await this.minioNestService.uploadSingle(file, dataPost);
+    let result = await this.minioNestService.upload(files, dataPost);
     res.status(result.statusCode).send(result);
   }
 
--- src/minio/minio.service.ts
+++ src/minio/minio.service.ts
@@ -5,107 +5,130 @@ import * as crypto from 'crypto';
 import { UploadPost, DownloadPost, ResultDownload } from './dto/minio.dto';
 @Injectable()
 export class MinioNestService {
-    constructor(private readonly minio: MinioService) { }
+  constructor(private readonly minio: MinioService) { }
 
-    private readonly expireTime = parseInt(process.env.MINIO_EXPIRE_TIME);
-    private readonly minio_key = process.env.MINIO_KEY;
-    public get client() {
-        return this.minio.client;
+  private readonly expireTime = parseInt(process.env.MINIO_EXPIRE_TIME);
+  private readonly minio_key = process.env.MINIO_KEY;
+  private readonly iv_key = "infomedianusanta"
+  public get client() {
+    return this.minio.client;
+  }
+
+  //MASTER FUNCTION
+  encrypt(data: string) {
+    crypto.createCipheriv
+    let mykey = crypto.createCipheriv('aes-128-cbc', this.minio_key, this.iv_key);
+    let mystr = mykey.update(data, 'utf8', 'hex');
+    mystr += mykey.final('hex');
+    return mystr;
+  }
+
+  decrypt(data: string) {
+    var mykey = crypto.createDecipheriv('aes-128-cbc', this.minio_key, this.iv_key);
+    var mystr = mykey.update(data, 'hex', 'utf8');
+    mystr += mykey.final('utf8');
+    return mystr;
+  }
+
+  async uploadSingle(
+    file: BufferedFile,
+    data: UploadPost,
+  ) {
+    const baseBucket = data.folder;
+
+    const checkBucket = await this.client.bucketExists(baseBucket)
+    if(!checkBucket){
+      await this.client.makeBucket(baseBucket, 'us-east-1');
     }
 
-    encrypt(data: string) {
-        let mykey = crypto.createCipher('aes-128-cbc', this.minio_key);
-        let mystr = mykey.update(data, 'utf8', 'hex');
-        mystr += mykey.final('hex');
-        return mystr;
+    let temp_filename = Date.now().toString();
+    let hashedFileName = crypto
+      .createHash('md5')
+      .update(temp_filename)
+      .digest('hex');
+    let ext = file.originalname.substring(
+      file.originalname.lastIndexOf('.'),
+      file.originalname.length,
+    );
+
+    const metaData = {
+      'Content-Type': file.mimetype,
+      'Origin-Filename': file.originalname,
+    };
+
+    let filename = hashedFileName + ext;
+    const fileName: string = `${data.directory}/${filename}`;
+    console.log("fileName",fileName)
+    const fileBuffer = file.buffer;
+    const fileSize = file.size;
+    const token = this.encrypt(`${baseBucket}:${fileName}`);
+    const resultUpload = await this.client.putObject(
+      data.folder,
+      fileName,
+      fileBuffer,
+      fileSize,
+      metaData,
+    );
+    const resultDownload = await this.client.presignedUrl(
+      'GET',
+      data.folder,
+      fileName,
+      this.expireTime,
+    );
+    let output = new ResultDownload();
+
+    output.fileName = filename;
+    output.mimeType = file.mimetype;
+    output.originFileName = file.originalname;
+    output.size = fileSize;
+    output.token = token;
+    output.url = resultDownload;
+    return output;
+  }
+  //END MASTER FUNCTION
+
+
+  async upload(files, data: UploadPost) {
+    try {
+      let output = []
+      for (let index = 0; index < files.length; index++) {
+        const resultUpload = await this.uploadSingle(files[index], data);
+        output.push(resultUpload)
+      }
+      // console.log(files);
+      return {
+        isError: false,
+        data: output,
+        statusCode: 201,
+      };
+    } catch (error) {
+      console.error(error);
+      return { isError: true, data: error.message, statusCode: 500 };
     }
-    decrypt(data: string) {
-        var mykey = crypto.createDecipher('aes-128-cbc', this.minio_key);
-        var mystr = mykey.update(data, 'hex', 'utf8');
-        mystr += mykey.final('utf8');
-        return mystr;
+  }
+
+  async download(post: DownloadPost) {
+    try {
+      const decryptToken = this.decrypt(post.token);
+      const decryptTokenArray = decryptToken.split(':');
+      const bucket = decryptTokenArray[0];
+      const filename = decryptTokenArray[1];
+      const resultDownload = await this.client.presignedUrl(
+        'GET',
+        bucket,
+        filename,
+        this.expireTime,
+      );
+      return {
+        isError: false,
+        data: {
+          url: resultDownload,
+        },
+        statusCode: 201,
+      };
+    } catch (error) {
+      console.error(error);
+      return { isError: true, data: error.message, statusCode: 500 };
     }
-
-    async uploadSingle(file: BufferedFile, data: UploadPost) {
-        try {
-            const baseBucket = data.folder;
-
-            this.client.makeBucket(baseBucket, 'us-east-1');
-
-            let temp_filename = Date.now().toString();
-            let hashedFileName = crypto
-                .createHash('md5')
-                .update(temp_filename)
-                .digest('hex');
-            let ext = file.originalname.substring(
-                file.originalname.lastIndexOf('.'),
-                file.originalname.length,
-            );
-
-            const metaData = {
-                'Content-Type': file.mimetype,
-                'Origin-Filename': file.originalname,
-            };
-
-            let filename = hashedFileName + ext;
-            const fileName: string = `${data.directory}/${filename}`;
-            const fileBuffer = file.buffer;
-            const fileSize = file.size;
-            const token = this.encrypt(`${baseBucket}:${fileName}`);
-            const resultUpload = await this.client.putObject(
-                data.folder,
-                fileName,
-                fileBuffer,
-                fileSize,
-                metaData,
-            );
-            const resultDownload = await this.client.presignedUrl(
-                'GET',
-                data.folder,
-                fileName,
-                this.expireTime,
-            );
-            let output = new ResultDownload();
-
-            output.fileName = filename;
-            output.mimeType = file.mimetype;
-            output.originFileName = file.originalname;
-            output.size = fileSize;
-            output.token = token;
-            output.url = resultDownload;
-            return {
-                isError: false,
-                data: output,
-                statusCode: 201,
-            };
-        } catch (error) {
-            console.error(error);
-            return { isError: true, data: error.message, statusCode: 500 };
-        }
-    }
-
-    async download(post: DownloadPost) {
-        try {
-            const decryptToken = this.decrypt(post.token);
-            const decryptTokenArray = decryptToken.split(':');
-            const bucket = decryptTokenArray[0];
-            const filename = decryptTokenArray[1];
-            const resultDownload = await this.client.presignedUrl(
-                'GET',
-                bucket,
-                filename,
-                this.expireTime,
-            );
-            return {
-                isError: false,
-                data: {
-                    url: resultDownload,
-                },
-                statusCode: 201,
-            };
-        } catch (error) {
-            console.error(error);
-            return { isError: true, data: error.message, statusCode: 500 };
-        }
-    }
+  }
 }
